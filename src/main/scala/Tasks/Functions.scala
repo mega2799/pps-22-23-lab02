e89f780e618e826efdc39cd0aca93212a7fb2832
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2023-03-13 16:38:38 +0100

    + task2 done

---
+++ src/main/scala/Tasks/Functions.scala
@@ -0,0 +1,32 @@
+package Tasks
+
+object Functions extends App
+
+    def toPositive(number : Int) : String = 
+        number match
+            case number if number < 0 => "negative"
+            case _ => "positive"
+
+    val positiveVal : Int => String= _ match
+        case n  => toPositive(n)
+    
+    def neg[X, Y](pred : X => Boolean) : X => Boolean =
+        (x : X) => !pred(x)
+
+    // val p1 : Int => Int => Int =  
+    //     x => y => z => (x > y && y == z) match
+    //         case true => 1
+    //         case false => 0
+
+    // val p2: <NonCurriedFunType> = ...
+
+    def p3(x : Int)(y : Int)(z : Int): Boolean =
+        x > y && y == z
+        
+    def p4(x : Int, y : Int, z : Int): Boolean = 
+        x > y && y == z
+
+    //il constraint e' il tipo degli argomenti, 
+    // in questo caso potrebbere essere anche 3 domini diversi
+    def compose[X, Y, Z ](f: Y => Z, g: X => Y ): X => Z =
+        i  => f(g(i))
---
+++ src/test/scala/u02/TaskFunctionsTest.scala
@@ -0,0 +1,63 @@
+package u02
+
+import junit.framework.TestCase.assertTrue
+import org.junit.Assert.{assertEquals, assertFalse}
+import org.junit.Test
+import Tasks.*
+import math.Fractional.Implicits.infixFractionalOps
+import math.Integral.Implicits.infixIntegralOps
+import math.Numeric.Implicits.infixNumericOps
+
+class TaskFunctionsTest:
+    // val functions = Functions
+
+    def toPositiveMethod() =
+        assertEquals("negative", toPositive(-22))
+        assertEquals("positive", toPositive(22))
+        assertEquals("positive", toPositive(0))
+
+    
+    def toPositiveVal() =
+        assertEquals("negative", positiveVal(-22))
+        assertEquals("positive", positiveVal(22))
+        assertEquals("positive", positiveVal(0))
+    
+    @Test
+    def checktoPositives() =
+        toPositiveMethod()
+        toPositiveVal()
+    
+    def negFunction() =
+        val empty: String => Boolean = _ == "" // predicate on strings
+        val notEmpty = neg(empty) // which type of notEmpty?
+        assertTrue(notEmpty("foo")) // true
+        assertFalse(notEmpty("")) // false
+    
+
+    @Test
+    def negTest()=
+        negFunction()
+ 
+    // def simpleCurry() = 
+        // assertEquals(1, p1(10, 3, 3))
+
+    def nonCurryDef() =
+        assertTrue(p4(10, 3, 3))
+        assertFalse(p4(10, 30, 30))
+        assertFalse(p4(40, 30, 10))
+    
+    def curryDef() =
+        assertTrue(p3(10)(3)(3))
+        assertFalse(p3(10)(30)(30))
+        assertFalse(p3(40)(30)(10))
+
+    @Test
+    def curryingTest()=
+        // simpleCurry()
+        curryDef()
+        nonCurryDef()
+
+    @Test
+    def composeTest() =
+        assertEquals(8.5, compose((i : Double)=> i - 1.5, (o : Double) => o * 2)(5.0), 0.0001)
+        assertEquals(1900L, compose((i : Long)=> i - 100L, (o : Long) => o * 2)(1000L), 0.0001)
